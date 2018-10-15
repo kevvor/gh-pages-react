72b0f8aff017c8f731dbe66924c10d4c242723dd
Keano <keanov@gmail.com>
2018-10-15 16:57:35 -0700

    Updates

---
+++ precache-manifest.aa0d399a230d97f778fe445bb7afa7ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0bb189cad76f66d445c6dc83c3550f35",
+    "url": "/gh-pages-react/static/media/text-tone.0bb189ca.mp3"
+  },
+  {
+    "revision": "9f39852713b79fbebfba",
+    "url": "/gh-pages-react/static/js/runtime~main.9f398527.js"
+  },
+  {
+    "revision": "3c547637c50f82461857",
+    "url": "/gh-pages-react/static/js/main.3c547637.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/gh-pages-react/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "3c547637c50f82461857",
+    "url": "/gh-pages-react/static/css/main.a37c1a19.chunk.css"
+  },
+  {
+    "revision": "c4f03245fdca747b5e9aea3b0f5e5d0a",
+    "url": "/gh-pages-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.ef7a4ad677a89ceb01a477b75e80d2d6.js"
+  "/gh-pages-react/precache-manifest.aa0d399a230d97f778fe445bb7afa7ea.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/me/index.html", {
+workbox.routing.registerNavigationRoute("/gh-pages-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
